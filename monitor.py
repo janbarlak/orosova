c395af34df35388864dce915441a04a2bad9abf3
Jan Barlak <47349005+janbarlak@users.noreply.github.com>
2026-09-10 16:11:40 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,5 +1,6 @@
 import json
 import urllib.request
+from pathlib import Path
 
 URL = "https://prixi.sk/professional/10/schedule_uid/5003255a-e77f-41be-bc14-96520975aace"
 
@@ -13,3 +14,8 @@ if isinstance(data, list) and len(data) == 0:
     print("NO_FREE_SLOTS")
 else:
     print("FREE_SLOTS_FOUND")
+
+    Path("issue.txt").write_text(
+        "Objavil sa aspoň jeden voľný termín v systéme Prixi.\n\n"
+        "https://prixi.sk/ordinacia/10/bot?fontSize=16&locale=sk"
+    )
